774d73d757af11aa3ce0c466d92d681b11e32d45
Deploy from Travis CI <>
2019-01-17 02:38:41 +0000

    Deploy ab3d280eca79d65b7445147e1e000a8d10434630 to gh-pages

---
+++ source-files.js
@@ -0,0 +1,2 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["flate2"] = {"name":"","dirs":[{"name":"deflate","dirs":[],"files":["bufread.rs","mod.rs","read.rs","write.rs"]},{"name":"gz","dirs":[],"files":["bufread.rs","mod.rs","read.rs","write.rs"]},{"name":"zlib","dirs":[],"files":["bufread.rs","mod.rs","read.rs","write.rs"]}],"files":["bufreader.rs","crc.rs","ffi.rs","lib.rs","mem.rs","zio.rs"]};
